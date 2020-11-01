f40435852a2eef7cfaa0a9da92782a69ea58482f
thevarunsharma <varunsharma123.del@gmail.com>
2020-11-01 23:26:41 +0530

    update cloth scoring

--- Cloth-Scoring/main.py
+++ Cloth-Scoring/main.py
@@ -11,29 +11,29 @@ app = Flask(__name__)
 @app.route("/for_clear", methods=["POST"])
 def upload_clear():
     if request.method == 'POST':
-        f1 = request.files.get('img1')
-        f2 = request.files.get('img2')
+        f1 = request.form.get('img1')
+        f2 = request.form.get('img2')
         if f2 is None or f1 is None:
         	return "No Image Uploaded!!!"
 
-        name1 = "img/" + secure_filename(f1.filename)
-        f1.save(name1)
-        name2 = "img/" + secure_filename(f2.filename)
-        f2.save(name2)
+        name1 = "../img/" + secure_filename(f1)
+        # f1.save(name1)
+        name2 = "../img/" + secure_filename(f2)
+        # f2.save(name2)
         score = float(compare_dresses_clear(name1, name2))
         return jsonify({"score" : score})
 
 @app.route("/for_mask",  methods=["POST"])
 def upload_unclear():
     if request.method == 'POST':
-        f1 = request.files.get('img1')
-        f2 = request.files.get('img2')
+        f1 = request.form.get('img1')
+        f2 = request.form.get('img2')
         if f2 is None or f1 is None:
         	return "No Image Uploaded!!!"
 
-        name1 = "img/" + secure_filename(f1.filename)
-        f1.save(name1)
-        name2 = "img/" + secure_filename(f2.filename)
-        f2.save(name2)
+        name1 = "../img/" + secure_filename(f1)
+        # f1.save(name1)
+        name2 = "../img/" + secure_filename(f2)
+        # f2.save(name2)
         score = float(compare_dresses_masked(name1, name2))
         return jsonify({"score" : score})
